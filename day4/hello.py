b0877217efabd266676687efd91d145929cb8a28
baski910 <baskar910@gmail.com>
2022-11-17 21:02:22 +0530

    module example 5

--- day4/hello.py
+++ day4/hello.py
@@ -1,5 +1,7 @@
 __all__ =['addnum','divnum'] # restricts function to be exported when 'from module import *' directive is used in calling script
+
 def addnum(a,b):
+    print(__name__)
     return a+b
 
 def divnum(a,b):
@@ -8,7 +10,9 @@ def divnum(a,b):
 #def sayhello():
 #    print("helloworld")
 
+# value for __name__ in the current script is __main__ and in calling script it is hello(module name)
+    
 if __name__ == '__main__':
-    print("calling from module:", __name__) # value for __name__ in the current script is __main__ and in calling script it is hello(module name)
+    print("calling from module:", __name__) 
     print("calling from module:",addnum(35,7))
     print("calling from module:",divnum(35,7))
