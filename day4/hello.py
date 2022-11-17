418792c5acc8d4ca7bffe99ac02bc3315515b9aa
baski910 <baskar910@gmail.com>
2022-11-17 20:43:46 +0530

    module example 4

--- day4/hello.py
+++ day4/hello.py
@@ -5,5 +5,10 @@ def addnum(a,b):
 def divnum(a,b):
     return a/b
 
-def sayhello():
-    print("helloworld")
+#def sayhello():
+#    print("helloworld")
+
+if __name__ == '__main__':
+    print("calling from module:", __name__) # value for __name__ in the current script is __main__ and in calling script it is hello(module name)
+    print("calling from module:",addnum(35,7))
+    print("calling from module:",divnum(35,7))
